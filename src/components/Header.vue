1154e9cbc731ae3c11495b58ce16ceec6799b360
Jorge <jorge.garcia.pastor@gmail.com>
2024-01-04 11:47:47 +0100

    [fix] fix mig header routes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,7 @@
 <script setup>
 import "./../assets/styles/components/Header.css";
+import logo from "../assets/img/logo.svg";
+import logoXS from "../assets/img/logo-xs.svg";
 defineProps({
   title: String,
 });
@@ -7,9 +9,9 @@ defineProps({
 <template>
   <header class="header">
     <picture>
-      <source srcSet="./img/logo.svg" media="(min-width: 600px)" />
-      <source srcSet="./img/logo-xs.svg" media="(max-width: 600px)" />
-      <img class="main-logo" src="./img/logo.svg" />
+      <source :srcSet="logo" media="(min-width: 600px)" />
+      <source :srcSet="logoXS" media="(max-width: 600px)" />
+      <img class="main-logo" :src="logo" />
     </picture>
     <h1>{{ title }}</h1>
   </header>
